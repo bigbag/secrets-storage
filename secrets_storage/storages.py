897cdbd0695b348ed2e360ea86c7843acb6f8588
Pavel Liashkov <pavel.liashkov@protonmail.com>
2021-01-30 18:57:26 +0200

    Refactoring in VaultStorage, add _get_client_token function

--- secrets_storage/storages.py
+++ secrets_storage/storages.py
@@ -1,7 +1,7 @@
 import abc
 import os
 from dataclasses import dataclass, field
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
 import hvac
 
@@ -30,7 +30,7 @@ class VaultStorage(BaseStorage):
     available: bool = False
     ssl_verify: bool = False
 
-    kube_token_path: str = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+    auth_token_path: str = "/var/run/secrets/kubernetes.io/serviceaccount/token"
 
     secrets: Dict[str, Any] = field(init=False, repr=False)
 
@@ -40,20 +40,25 @@ class VaultStorage(BaseStorage):
         else:
             self.secrets = self._get_secrets()
 
-    def _get_secrets(self):
-        with open(self.kube_token_path) as f:
-            client = hvac.Client(url=self.host, verify=self.ssl_verify)
-            auth_info = client.auth_kubernetes(role=self.role, jwt=f.read())
-            token = auth_info.get("auth", {}).get("client_token")
-            if not token:
-                raise ValueError("Not found vault token.")
+    def _get_client_token(self, client: hvac.Client) -> str:
+        with open(self.auth_token_path) as f:
+            sa_token = f.read()
 
-            client.token = token
-            return client.read(self.namespace).get("data", {})
+        auth_info = client.auth_kubernetes(role=self.role, jwt=sa_token)
+        client_token = auth_info.get("auth", {}).get("client_token")
+        if not client_token:
+            raise ValueError("Not found vault token.")
+
+        return str(client_token)
+
+    def _get_secrets(self):
+        client = hvac.Client(url=self.host, verify=self.ssl_verify)
+        client.token = self._get_client_token(client)
+        return client.read(self.namespace).get("data", {})
 
     @property
     def enabled(self) -> bool:
-        return bool(self.available and self.host and self.kube_token_path and self.namespace)
+        return bool(self.available and self.host and self.auth_token_path and self.namespace)
 
     def get_secret(self, name: str, fallback_value: Any = None) -> Any:
         return self.secrets.get(name)
